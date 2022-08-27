83cf26eeaff5047f02b53f5594b657d943781819
gmark <g.markn2003@gmail.com>
2022-08-27 18:37:52 +0300

    task5-page5

---
+++ main.py
@@ -0,0 +1,17 @@
+flag = 0
+inputnum = int(input("Enter Number: "))
+inputdigit = int((input("Enter Digit: ")))
+checker = int(inputnum)
+falser = int(inputnum)  # mirroring number
+while falser != 0:
+    checker = falser % 10
+    if checker == inputdigit:
+        flag = 1
+    falser = falser / 10
+
+if flag == 1:
+    print("Approved")
+if flag == 0:
+    changer = inputnum % 10
+    inputnum = (inputnum - changer) + inputdigit
+    print("New Num Is: ", inputnum)
